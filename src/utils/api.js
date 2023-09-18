04255603a2ae8ac91ac92a56190b6ba4c6efa399
aries grey <ariesgrey88@gmail.com>
2023-09-17 22:33:28 -0500

    added backend connection

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL = "https://welovemovies-backend-2slh.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
@@ -23,42 +22,42 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options, onCancel) {
-  try {
-    const response = await fetch(url, options);
+	try {
+		const response = await fetch(url, options);
 
-    if (response.status === 204) {
-      return null;
-    }
+		if (response.status === 204) {
+			return null;
+		}
 
-    const payload = await response.json();
+		const payload = await response.json();
 
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
-    }
-    return Promise.resolve(onCancel);
-  }
+		if (payload.error) {
+			return Promise.reject({ message: payload.error });
+		}
+		return payload.data;
+	} catch (error) {
+		if (error.name !== "AbortError") {
+			console.error(error.stack);
+			throw error;
+		}
+		return Promise.resolve(onCancel);
+	}
 }
 
 function populateReviews(signal) {
-  return async (movie) => {
-    const url = `${API_BASE_URL}/movies/${movie.movie_id}/reviews`;
-    movie.reviews = await fetchJson(url, { headers, signal }, []);
-    return movie;
-  };
+	return async (movie) => {
+		const url = `${API_BASE_URL}/movies/${movie.movie_id}/reviews`;
+		movie.reviews = await fetchJson(url, { headers, signal }, []);
+		return movie;
+	};
 }
 
 function populateTheaters(signal) {
-  return async (movie) => {
-    const url = `${API_BASE_URL}/movies/${movie.movie_id}/theaters`;
-    movie.theaters = await fetchJson(url, { headers, signal }, []);
-    return movie;
-  };
+	return async (movie) => {
+		const url = `${API_BASE_URL}/movies/${movie.movie_id}/theaters`;
+		movie.theaters = await fetchJson(url, { headers, signal }, []);
+		return movie;
+	};
 }
 
 /**
@@ -67,11 +66,11 @@ function populateTheaters(signal) {
  *  a promise that resolves to a possibly empty array of movies saved in the database.
  */
 export async function listMovies(signal) {
-  const url = new URL(`${API_BASE_URL}/movies?is_showing=true`);
-  const addReviews = populateReviews(signal);
-  return await fetchJson(url, { headers, signal }, []).then((movies) =>
-    Promise.all(movies.map(addReviews))
-  );
+	const url = new URL(`${API_BASE_URL}/movies?is_showing=true`);
+	const addReviews = populateReviews(signal);
+	return await fetchJson(url, { headers, signal }, []).then((movies) =>
+		Promise.all(movies.map(addReviews))
+	);
 }
 
 /**
@@ -80,8 +79,8 @@ export async function listMovies(signal) {
  *  a promise that resolves to a possibly empty array of theaters saved in the database.
  */
 export async function listTheaters(signal) {
-  const url = new URL(`${API_BASE_URL}/theaters`);
-  return await fetchJson(url, { headers, signal }, []);
+	const url = new URL(`${API_BASE_URL}/theaters`);
+	return await fetchJson(url, { headers, signal }, []);
 }
 
 /**
@@ -90,25 +89,25 @@ export async function listTheaters(signal) {
  *  a promise that resolves to a possibly empty array of movies saved in the database.
  */
 export async function readMovie(movieId, signal) {
-  const url = new URL(`${API_BASE_URL}/movies/${movieId}`);
-  const addReviews = populateReviews(signal);
-  const addTheaters = populateTheaters(signal);
-  return await fetchJson(url, { headers, signal }, [])
-    .then(addReviews)
-    .then(addTheaters);
+	const url = new URL(`${API_BASE_URL}/movies/${movieId}`);
+	const addReviews = populateReviews(signal);
+	const addTheaters = populateTheaters(signal);
+	return await fetchJson(url, { headers, signal }, [])
+		.then(addReviews)
+		.then(addTheaters);
 }
 
 export async function deleteReview(reviewId) {
-  const url = `${API_BASE_URL}/reviews/${reviewId}`;
-  return await fetchJson(url, { method: "DELETE", headers }, {});
+	const url = `${API_BASE_URL}/reviews/${reviewId}`;
+	return await fetchJson(url, { method: "DELETE", headers }, {});
 }
 
 export async function updateReview(reviewId, data) {
-  const url = `${API_BASE_URL}/reviews/${reviewId}`;
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify({ data }),
-  };
-  return await fetchJson(url, options, {});
+	const url = `${API_BASE_URL}/reviews/${reviewId}`;
+	const options = {
+		method: "PUT",
+		headers,
+		body: JSON.stringify({ data }),
+	};
+	return await fetchJson(url, options, {});
 }
